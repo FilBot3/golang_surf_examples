c0547180fb032c64ca1d41c70a89cc895a6e7c47
Predatorian <Predatorian3@gmail.com>
2017-09-15 21:57:58 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/PuerkitoBio/goquery"
+	"gopkg.in/headzoo/surf.v1"
+)
+
+func checkerr(err error) {
+  if err != nil{
+    panic(err)
+  }
+}
+
+func main() {
+	bow := surf.NewBrowser()
+	err := bow.Open("http://google.com")
+  checkerr(err)
+
+	fmt.Println(bow.Title())
+  // https://www.sitepoint.com/comprehensive-jquery-selectors/ => Look for Attribute Selectors.
+	search, err := bow.Form("[name='f']")
+  checkerr(err)
+  search.Input("q", "Cheese")
+  err = search.Submit()
+  checkerr(err)
+  fmt.Println(bow.Body())
+}
